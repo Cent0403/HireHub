0664f79d8e6739766a8917749a293196bb6acbb2
Luis José Rodríguez Centeno <luis.rodriguez7@catolica.edu.sv>
2025-11-19 15:40:17 -0600

    Delete scripts

--- hirehub_backend/scripts/add-cv-column.sql
+++
@@ -1,5 +0,0 @@
--- Agregar columna para almacenar la ruta del CV en PDF
-ALTER TABLE detalles_candidato 
-ADD COLUMN IF NOT EXISTS cv_url VARCHAR(500);
-
-COMMENT ON COLUMN detalles_candidato.cv_url IS 'Ruta del archivo PDF del curriculum vitae del candidato';
--- hirehub_backend/scripts/fix-detalles-candidato.sql
+++
@@ -1,3 +0,0 @@
--- Hacer que la columna nacionalidad sea nullable
-ALTER TABLE detalles_candidato 
-ALTER COLUMN nacionalidad DROP NOT NULL;
